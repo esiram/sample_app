6d0dce87de202cde0cabbefb2044b922ecf007cd
Esther Siram <isusecalea@yahoo.com>
2017-09-03 17:03:29 -0500

    Implement basic login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
   test "invalid signup information" do
-
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name: "",
@@ -15,6 +14,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
 
   test "valid signup information" do
+    User.create()
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: {user: { name: "Example User",
@@ -24,6 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
     assert_not flash.nil?
   end
 
